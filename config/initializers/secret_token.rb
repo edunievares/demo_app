d88362a91def355b058b25ba4366048bb2c19edf
edunievares <edu.nievares@gmail.com>
2016-08-03 12:50:37 +0200

    Fix weird problem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '572209a0d58c7e058eaf22849c52dbf9dc1d9975fd02b81abf8d5c175ff19f1bdfb1f0d07d9a9e883676360944ae7ab400ea26cb7c05d3e0fe0ea3a00f888373'
